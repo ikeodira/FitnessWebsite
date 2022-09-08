7f9ea9123151b419fc8f7b3d9948cc394adf56b8
Ike Odira <odiraike8@gmail.com>
2022-09-08 16:38:47 +0100

    Create Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,19 +14,19 @@ function Navbar() {
   return (
     <nav>
         <div className="container nav__container">
-            <Link to="/" className="logo">
+            <Link to="/" className="logo" onClick={()=> setIsNavShowing(false)}>
                 <img src={Logo} alt="Nav logo"/>
             </Link>
             <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__Nav'}`}>
                 {links.map(({name, path}, index)=>{
                     return (
                         <li key={index}>
-                            <NavLink to={path} className={({isActive}) => isActive ? "active-nav": ""}>{name}</NavLink>
+                            <NavLink to={path} className={({isActive}) => isActive ? "active-nav": ""} onClick={()=> setIsNavShowing( prev => !prev)}>{name}</NavLink>
                         </li>
                     )
                 })}
             </ul>
-            <button className="nav__toggle-btn" onClick={()=> setIsNavShowing(!isNavShowing)}>
+            <button className="nav__toggle-btn" onClick={()=> setIsNavShowing( prev => !prev)}>
                 {isNavShowing ?  <MdOutlineClose/>:  <GoThreeBars/>}                 
             </button>
         </div>
