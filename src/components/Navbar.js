da236634b63281f882f80682582d6a732ae8a297
Ike Odira <odiraike8@gmail.com>
2022-09-08 14:09:45 +0100

    delete default favicon

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../images/logo.png";
+import {links} from "../data";
+import {GoThreeBars} from "react-icons/go"
+import {MdOutlineClose} from "react-icons/md";
+import "./navbar.css";
+
+function Navbar() {
+
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+
+  return (
+    <nav>
+        <div className="container nav__container">
+            <Link to="/" className="logo">
+                <img src={Logo} alt="Nav logo"/>
+            </Link>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__Nav'}`}>
+                {links.map(({name, path}, index)=>{
+                    return (
+                        <li key={index}>
+                            <NavLink to={path} className={({isActive}) => isActive ? "active-nav": ""}>{name}</NavLink>
+                        </li>
+                    )
+                })}
+            </ul>
+            <button className="nav__toggle-btn" onClick={()=> setIsNavShowing(!isNavShowing)}>
+                {isNavShowing ?  <MdOutlineClose/>:  <GoThreeBars/>}                 
+            </button>
+        </div>
+    </nav>    
+  )
+}
+
+export default Navbar
